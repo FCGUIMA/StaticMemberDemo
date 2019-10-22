98e3bf2fa5f514c63828068b557197755fc298d3
Fernando Guimaraes <fc.guimaraes@uol.com.br>
2019-10-22 17:43:01 -0300

    Commit grande

--- src/staticmemberdemo/ItemSizes.java
+++ src/staticmemberdemo/ItemSizes.java
@@ -3,7 +3,7 @@ package staticmemberdemo;
 public class ItemSizes {
 	public static String mSmall = "Men's Small";
 	public static String mMedium = "Men's Medium";
-	public static String mLarge = "Men's Large";
+	public static String mLarge = "Men's Grande";
 	
 	public static String ListAllSizes() {
 		return mSmall + ", " + mMedium + ", " + mLarge;
